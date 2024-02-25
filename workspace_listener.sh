4e6672e618c7141e72674599383b176ab6398bd0
CarloCattano <freevibes@gmail.com>
2024-02-25 22:45:00 +0100

    remove on close too

--- workspace_listener.sh
+++ workspace_listener.sh
@@ -25,6 +25,7 @@ handle() {
   case $1 in
       workspace*) screen_shot ;;
       createworkspace*) screen_shot ;;
+      closewindow*) rm_ws "$(echo $1 | cut -d '>' -f 3)" ;;
       destroyworkspace*) rm_ws "$(echo $1 | cut -d '>' -f 3)" ;;
   esac
 }
